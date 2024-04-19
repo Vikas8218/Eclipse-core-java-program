ceabae53d115b0d0bad35058444dd9756ca2b31b
Vikas Saini <vikas08218@gmail.com>
2024-04-19 11:44:16 +0530

    java program submit corejava

---
+++ src/com/coding/Stringreverse.java
@@ -0,0 +1,15 @@
+package com.coding;
+
+public class Stringreverse {
+
+	public static void main(String[] args) {
+		String str ="vikassaini";
+		String rev ="";
+		for(int i=str.length()-1;i>=0;i--) {
+			rev =rev+str.charAt(i);
+		}
+		System.out.println(rev);
+
+	}
+
+}
