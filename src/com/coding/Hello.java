13a74803e88338e89de35e24e1e9205c9ad609db
Vikas Saini <vikas08218@gmail.com>
2024-04-27 14:22:30 +0530

    submit

--- src/com/coding/Hello.java
+++ src/com/coding/Hello.java
@@ -3,8 +3,15 @@ package com.coding;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello world!");
+		int n=5;
+		int k=1;
+		for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n;j++) {
+			System.out.printf("%2d ",k);
+			k++;
+		}
+		System.out.println();
+		}
 	}
 
 }
